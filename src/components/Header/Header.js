0543835ace25232064b635e98674f186b2295545
akmsyfulalom <akmsyfulbd12@gmail.com>
2022-09-30 21:51:27 +0600

    user section complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import logo from '../../images/logo.png'
 const Header = () => {
     return (
-        <div className='flex mt-5 ml-20'>
+        <div className='flex py-10 ml-20'>
             <img className='w-20' src={logo} alt="" />
             <h1 className='text-5xl font-bold text-slate-700'>Daily Runner planner</h1>
         </div>
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import user from '../../images/user.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import './User.css'
 
 const User = () => {
     return (
-        <div>
+        <div className='bg-white p-10 rounded user'>
             <div className='flex '>
                 <img className='w-14 rounded-full mr-4' src={user} alt="" />
                 <div>
                     <h1 className='font-semibold text-2xl	' >AKM SYFUL</h1>
-                    <p>Sylhet, BD</p>
+                    <p><FontAwesomeIcon icon={faLocationDot} /> Sylhet, BD</p>
+                    <p>  </p>
                 </div>
 
             </div>
+            <div className='flex mt-10 justify-between  px-5 py-3 bg-[#F2F4FA] rounded'>
+                <div className='mr-5'>
+                    <p><span className='font-bold text-2xl	'>75</span>kg</p>
+                    <p>Weight</p>
+                </div>
+                <div className='mr-5'>
+                    <p className='font-bold text-2xl'>6.5</p>
+                    <p>Height</p>
+                </div>
+                <div>
+                    <p><span className='font-bold text-2xl'>25</span>yer</p>
+                    <p>Age</p>
+                </div>
+            </div>
+            <h1 className='my-3 font-bold text-2xl'>Add A Break
+            </h1>
+            <div className='flex bg-[#F2F4FA] px-5 py-3 rounded'>
+                <button><p className='mr-3 bg-white p-3 rounded-full hover:bg-blue-600 hover:text-white '><span className='font-bold'>10</span>S</p></button>
+                <button><p className='mr-3 bg-white p-3 rounded-full hover:bg-blue-600 hover:text-white '><span className='font-bold'>20</span>S</p></button>
+                <button><p className='mr-3 bg-white p-3 rounded-full hover:bg-blue-600 hover:text-white'><span className='font-bold'>30</span>S</p></button>
+                <button><p className='mr-3 bg-white p-3 rounded-full hover:bg-blue-600 hover:text-white'><span className='font-bold'>40</span>S</p></button>
+                <button><p className='mr-3 bg-white p-3 rounded-full hover:bg-blue-600 hover:text-white'><span className='font-bold'>50</span>S</p></button>
+            </div>
+
+            <h1 className='my-3 font-bold text-2xl'>Exercise Details
+            </h1>
+            <div className='flex justify-between px-5 py-5 rounded bg-[#F2F4FA]'>
+                <h1 className='font-bold text-xl'>Exercise time</h1>
+                <p className='text-slate-400'>00 minutes</p>
+            </div>
+            <div className='mt-10'>
+                <div className='flex justify-between px-5 py-5 rounded bg-[#F2F4FA]'>
+                    <h1 className='font-bold text-xl'>Break time</h1>
+                    <p className='text-slate-400'>00 Seconds</p>
+                </div>
+            </div>
+            <button className='font-bold  bg-blue-500  mt-5  py-3 px-24 rounded items-center '>Activity Completed
+            </button>
         </div>
     );
 };
